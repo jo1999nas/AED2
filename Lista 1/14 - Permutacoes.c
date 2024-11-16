7bc74459d0f53d77c43ceaed4f11542a90525bc8
jo1999nas <79485680+jo1999nas@users.noreply.github.com>
2024-11-16 20:27:15 -0300

    Update 14 - Permutacoes.c

--- Lista 1/14 - Permutacoes.c
+++ Lista 1/14 - Permutacoes.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
+// O numero de permutações de um vetor com n elementos é n!
+
 // n = quantidade de numeros disponiveis
-// k = quantidade de digitos da permutação
 // seq = vetor com a permutação em construção
 // usado = vetor que guarda se um número foi usado na permutação
 // indice = indice atual dentro de seq
-void gerar_permutacoes(int n, int k, int *seq, int *usado, int indice) {
+
+void gerar_permutacoes(int n, int *seq, int *usado, int indice) {
     // Se a permutação está completa, imprime seq e retorna
-    if (indice == k) {
-        for (int j = 0; j < k; j++) {
+    if (indice == n) {
+        for (int j = 0; j < n; j++) {
             printf("%d ", seq[j]);
         }
         printf("\n");
@@ -23,7 +26,7 @@ void gerar_permutacoes(int n, int k, int *seq, int *usado, int indice) {
             // Marca o numero v como usado
             usado[v] = 1;
             // Chama recursivamente a função
-            gerar_permutacoes(n, k, seq, usado, indice + 1);
+            gerar_permutacoes(n, seq, usado, indice + 1);
             // Desfaz a marcação (backtracking)
             usado[v] = 0;
         }
@@ -31,27 +34,19 @@ void gerar_permutacoes(int n, int k, int *seq, int *usado, int indice) {
 }
 
 int main() {
-    int n, k;
+    int n;
     printf("Digite o valor de n (quantidade de números): ");
     scanf("%d", &n);
-    printf("Digite o valor de k (comprimento da permutação): ");
-    scanf("%d", &k);
-
-    if (k > n || n <= 0 || k <= 0) {
-        printf("Valores inválidos! Certifique-se de que 0 < k <= n.\n");
-        return 1;
-    }
 
     // Declaração dos vetores
-    int seq[k];
+    int seq[n];
     int usado[n + 1];
     // Inicializando o vetor usado
     for (int i = 0; i <= n; i++) {
         usado[i] = 0;
     }
 
-    printf("Gerando permutações de %d números com comprimento %d:\n", n, k);
-    gerar_permutacoes(n, k, seq, usado, 0);
+    gerar_permutacoes(n, seq, usado, 0);
 
     return 0;
 }
