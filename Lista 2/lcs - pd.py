2abd87f9699c17728f3d3bf304f5726d60687c05
jo1999nas <79485680+jo1999nas@users.noreply.github.com>
2025-01-06 13:16:43 -0300

    Create lcs - pd.py

---
+++ Lista 2/lcs - pd.py
@@ -0,0 +1,12 @@
+def lcs(p, q, n, m):
+    if memo[n][m] != -1: return memo[n][m]
+    if n == 0 or m == 0:
+        resultado = 0
+    elif p[n-1] == q[m-1]:
+        resultado = 1 + lcs(p, q, n - 1, m - 1)
+    else:
+        temp1 = lcs(p, q, n - 1, m)
+        temp2 = lcs(p, q, n, m - 1)
+        resultado = max(temp1, temp2)
+        memo[n][m] = resultado
+    return resultado
